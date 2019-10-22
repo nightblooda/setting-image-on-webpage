7f35b639e5919e6dbc4aa876a7c5bee8a2b8c4e9
Himanshu Choudhary <himanshux7x7x7@gmail.com>
2019-10-23 00:13:44 +0530

    Completing image_gird.

--- grid_flex.js
+++ grid_flex.js
@@ -20,10 +20,12 @@ function four(){
     elements[i].style.flex= '25%';
   }
 }
-
-function More(){
-  for(i= 0;i< elements.length; i++){
-    elements[i].style.msFlex= '10%';
-    elements[i].style.flex= '10%';
-  }
+var header= document.getElementById('myHeader');
+var btns= header.getElementsByClassName('btn');
+for(var i= 0; i< btns.length; i++){
+  btns[i].addEventListener('click', function(){
+    var current= document.getElementsByClassName('active');
+    current[0].className= current[0].className.replace('active', '');
+    this.className+= ' active';
+  });
 }
